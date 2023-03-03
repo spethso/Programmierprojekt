bbde4a27563b4c433523851794a66081eeb5bbbf
Sandro Speth <sandro.speth@iste.uni-stuttgart.de>
2023-03-03 20:52:47 +0100

    Add additional packages and main classes for additional tasks

---
+++ Exercise-Sheet/src/main/java/main/java/de/phl/programmingproject/payrollsystem/PayrollSystem.java
@@ -0,0 +1,7 @@
+package main.java.de.phl.programmingproject.payrollsystem;
+
+public class PayrollSystem {
+    public static void main(String[] args) {
+        // TODO implement this operation
+    }
+}
